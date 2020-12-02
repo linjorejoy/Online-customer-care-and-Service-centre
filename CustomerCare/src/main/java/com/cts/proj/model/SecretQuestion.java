1eede61f04a0180e9c2ec237be8e618c0fa096ce
shabeerali-kh <shabeeralikh@gmail.com>
2020-12-02 16:36:43 +0530

    secretQuestion updated

--- CustomerCare/src/main/java/com/cts/proj/model/SecretQuestion.java
+++ CustomerCare/src/main/java/com/cts/proj/model/SecretQuestion.java
@@ -2,4 +2,40 @@ package com.cts.proj.model;
 
 public class SecretQuestion {
 
+	private long userSqId;
+	private long questionId;
+	private long userId;
+	private String answer;
+
+	public long getUserSqId() {
+		return userSqId;
+	}
+
+	public void setUserSqId(long userSqId) {
+		this.userSqId = userSqId;
+	}
+
+	public long getQuestionId() {
+		return questionId;
+	}
+
+	public void setQuestionId(long questionId) {
+		this.questionId = questionId;
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
 }
