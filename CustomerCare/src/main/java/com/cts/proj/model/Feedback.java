78fbde7f432c13d0a486fe2e4f4e33ddd8cea140
jishnuP7 <73280575+jishnuP7@users.noreply.github.com>
2020-12-02 15:33:14 +0530

    feedback added

--- CustomerCare/src/main/java/com/cts/proj/model/Feedback.java
+++ CustomerCare/src/main/java/com/cts/proj/model/Feedback.java
@@ -1,5 +1,36 @@
 package com.cts.proj.model;
 
 public class Feedback {
-
+	int responseId;
+	int questionId;
+	int complaintId;
+	String answer;
+	
+	public Feedback() {
+		super();
+	} 
+	public int getResponseId() {
+		return responseId;
+	}
+	public void setResponseId(int responseId) {
+		this.responseId = responseId;
+	}
+	public int getQuestionId() {
+		return questionId;
+	}
+	public void setQuestionId(int questionId) {
+		this.questionId = questionId;
+	}
+	public int getComplaintId() {
+		return complaintId;
+	}
+	public void setComplaintId(int complaintId) {
+		this.complaintId = complaintId;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
 }
