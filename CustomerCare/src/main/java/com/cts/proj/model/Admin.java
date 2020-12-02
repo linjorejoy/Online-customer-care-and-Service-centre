2642549ab11a1a2983aaeb9e1b1f97b88649b335
shabeerali-kh <shabeeralikh@gmail.com>
2020-12-02 15:04:05 +0530

    admin model class completed

--- CustomerCare/src/main/java/com/cts/proj/model/Admin.java
+++ CustomerCare/src/main/java/com/cts/proj/model/Admin.java
@@ -2,4 +2,98 @@ package com.cts.proj.model;
 
 public class Admin {
 
+	private long adminId;
+	private String password;
+	private String tempPassword;
+	private String firstName;
+	private String lastName;
+	private String emailId;
+
+	public Admin(long adminId, String password, String tempPassword, String firstName, String lastName,
+			String emailId) {
+		super();
+		this.adminId = adminId;
+		this.password = password;
+		this.tempPassword = tempPassword;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+	}
+
+	public long getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(long adminId) {
+		this.adminId = adminId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getTempPassword() {
+		return tempPassword;
+	}
+
+	public void setTempPassword(String tempPassword) {
+		this.tempPassword = tempPassword;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	@Override
+	public String toString() {
+		return "Admin [adminId=" + adminId + ", firstName=" + firstName + ", lastName=" + lastName + ", emailId="
+				+ emailId + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (adminId ^ (adminId >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Admin other = (Admin) obj;
+		if (adminId != other.adminId)
+			return false;
+		return true;
+	}
+
 }
