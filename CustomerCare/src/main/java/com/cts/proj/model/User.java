60812d945bee0a603f588acd71700dfb5f3c5852
AmazonianPanda <howyoudoing1618@gmail.com>
2020-12-03 22:55:09 +0530

    user class spacing corrected

--- CustomerCare/src/main/java/com/cts/proj/model/User.java
+++ CustomerCare/src/main/java/com/cts/proj/model/User.java
@@ -3,140 +3,140 @@ package com.cts.proj.model;
 import java.util.Date;
 
 public class User implements Comparable<User> {
-    private long userId;
-    private String password;
-    private String tempPassword;
-    private String firstName;
-    private String lastName;
-    private long phoneNumber;
-    private String emailId;
-    private Date dateOfBirth;
-    private String gender;
+	private long userId;
+	private String password;
+	private String tempPassword;
+	private String firstName;
+	private String lastName;
+	private long phoneNumber;
+	private String emailId;
+	private Date dateOfBirth;
+	private String gender;
 
-    public User() {
-        super();
-    }
+	public User() {
+		super();
+	}
 
-    public User(long userId, String firstName, String lastName, long phoneNumber, String emailId, Date dateOfBirth,
-            String gender) {
-        super();
-        this.userId = userId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phoneNumber = phoneNumber;
-        this.emailId = emailId;
-        this.dateOfBirth = dateOfBirth;
-        this.gender = gender;
-    }
+	public User(long userId, String firstName, String lastName, long phoneNumber, String emailId, Date dateOfBirth,
+			String gender) {
+		super();
+		this.userId = userId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.emailId = emailId;
+		this.dateOfBirth = dateOfBirth;
+		this.gender = gender;
+	}
 
-    public long getUserId() {
-        return userId;
-    }
+	public long getUserId() {
+		return userId;
+	}
 
-    public void setUserId(long userId) {
-        this.userId = userId;
-    }
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public String getTempPassword() {
-        return tempPassword;
-    }
+	public String getTempPassword() {
+		return tempPassword;
+	}
 
-    public void setTempPassword(String tempPassword) {
-        this.tempPassword = tempPassword;
-    }
+	public void setTempPassword(String tempPassword) {
+		this.tempPassword = tempPassword;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public long getPhoneNumber() {
-        return phoneNumber;
-    }
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
 
-    public void setPhoneNumber(long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 
-    public String getEmailId() {
-        return emailId;
-    }
+	public String getEmailId() {
+		return emailId;
+	}
 
-    public void setEmailId(String emailId) {
-        this.emailId = emailId;
-    }
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
 
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
 
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
 
-    public String getGender() {
-        return gender;
-    }
+	public String getGender() {
+		return gender;
+	}
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
 
-    @Override
-    public String toString() {
-        return "User [userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName + ", phoneNumber="
-                + phoneNumber + ", emailId=" + emailId + ", dateOfBirth=" + dateOfBirth + ", gender=" + gender + "]";
-    }
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName + ", phoneNumber="
+				+ phoneNumber + ", emailId=" + emailId + ", dateOfBirth=" + dateOfBirth + ", gender=" + gender + "]";
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + (int) (userId ^ (userId >>> 32));
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (userId ^ (userId >>> 32));
+		return result;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        User other = (User) obj;
-        if (userId != other.userId)
-            return false;
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (userId != other.userId)
+			return false;
+		return true;
+	}
 
-    @Override
-    public int compareTo(User userObj) {
-        if (this.userId == userObj.getUserId()) {
-            return 0;
-        } else if (this.userId > userObj.getUserId()) {
-            return 1;
-        }
-        return -1;
-    }
+	@Override
+	public int compareTo(User userObj) {
+		if (this.userId == userObj.getUserId()) {
+			return 0;
+		} else if (this.userId > userObj.getUserId()) {
+			return 1;
+		}
+		return -1;
+	}
 
 }
