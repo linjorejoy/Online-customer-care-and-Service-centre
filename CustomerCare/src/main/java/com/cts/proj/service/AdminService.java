9d3099ddb1de01efad88ace531f35f8b6e3c9b3c
Anusha <anushaganesh23@gmail.com>
2020-12-04 17:04:49 +0530

    AdminService

--- CustomerCare/src/main/java/com/cts/proj/service/AdminService.java
+++ CustomerCare/src/main/java/com/cts/proj/service/AdminService.java
@@ -1,5 +1,23 @@
 package com.cts.proj.service;
 
-public class AdminService {
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cts.proj.model.Admin;
+import com.cts.proj.repository.AdminRepository;
+
+@Service
+public class AdminService {
+   @Autowired
+   private AdminRepository adminRepo;
+   
+   public Admin getAdmin(long adminId) {
+	   return adminRepo.getOne(adminId);
+   }
+   public String getAdminPassword(long adminId) {
+	   Admin admin =  adminRepo.getOne(adminId);
+	   return admin.getPassword();
+	   
+   }
 }
