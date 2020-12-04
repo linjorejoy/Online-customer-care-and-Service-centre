4a41f64041d391e804bb82bfe6a6340370ccb9b9
AmazonianPanda <howyoudoing1618@gmail.com>
2020-12-04 11:52:19 +0530

    validators added, login controller

--- CustomerCare/src/main/java/com/cts/proj/controller/LoginController.java
+++ CustomerCare/src/main/java/com/cts/proj/controller/LoginController.java
@@ -1,5 +1,49 @@
 package com.cts.proj.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.cts.proj.model.Admin;
+import com.cts.proj.model.Analyst;
+import com.cts.proj.model.User;
+
+@Controller
 public class LoginController {
 
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String userLogin(@Validated @ModelAttribute("user")  User user, BindingResult result ) {
+		
+		if(result.hasErrors()) {
+			return "user-login";
+		}
+		
+		
+		
+		return "complaint-creation";
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String adminLogin(@Validated @ModelAttribute("admin") Admin admin, BindingResult result ) {
+
+		if(result.hasErrors()) {
+			return "admin-login";
+		}
+		
+		return "";
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String analystLogin(@Validated @ModelAttribute("analyst") Analyst analyst, BindingResult result ) {
+
+		if(result.hasErrors()) {
+			return "analyst-login";
+		}
+		
+		return "";
+	}
+	
 }
