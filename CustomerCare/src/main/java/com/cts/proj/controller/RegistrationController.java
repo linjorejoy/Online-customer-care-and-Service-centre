f0b5d25ee58ab243b090a257fd7a96d8a1337952
shabeerali-kh <shabeeralikh@gmail.com>
2020-12-04 12:45:16 +0530

    registration controller added

--- CustomerCare/src/main/java/com/cts/proj/controller/RegistrationController.java
+++ CustomerCare/src/main/java/com/cts/proj/controller/RegistrationController.java
@@ -2,6 +2,9 @@ package com.cts.proj.controller;
 
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 
 import org.springframework.validation.BindingResult;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.cts.proj.model.Analyst;
+import com.cts.proj.model.User;
 
 
 @Controller
@@ -27,5 +31,33 @@ public class RegistrationController {
 			return "analyst-reg";
 		}
 		return "anayst-login";
+	}
+	
+	@RequestMapping(value = "/userRegistration", method = RequestMethod.GET)
+	public String userRegistration(@ModelAttribute User user) {
+		return "user-reg";
+	}
+	@RequestMapping(value="/registerUser" , method=RequestMethod.GET)
+	public String registerUser(@Validated @ModelAttribute User user, BindingResult result) {
+		if(result.hasErrors()) {
+			return "user-reg";
+		}
+		return "user-login";
+	}
+	
+	@ModelAttribute("genderList")
+	public List<String> populateGender(){
+		List<String> genderList=new ArrayList<String>();	
+		genderList.add("Male");
+		genderList.add("Female");
+		return genderList;
+	}
+	@ModelAttribute("supportLevel")
+	public List<String> populateSupportLevel(){
+		List<String> supportLevel=new ArrayList<String>();	
+		supportLevel.add("L1");
+		supportLevel.add("L2");
+		supportLevel.add("L3");
+		return supportLevel;
 	}
 }
