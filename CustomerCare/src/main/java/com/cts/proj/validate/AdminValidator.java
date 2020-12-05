71befdc6a2f9b37947def72bfcf7a96d4c587fdd
Anusha <anushaganesh23@gmail.com>
2020-12-05 13:12:45 +0530

    AdminValidator complaint-notification

--- CustomerCare/src/main/java/com/cts/proj/validate/AdminValidator.java
+++ CustomerCare/src/main/java/com/cts/proj/validate/AdminValidator.java
@@ -1,19 +1,35 @@
 package com.cts.proj.validate;
 
+import java.security.NoSuchAlgorithmException;
+
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
-public class AdminValidator implements Validator {
+import com.cts.proj.model.Admin;
+import com.cts.proj.security.SecureWithSHA256;
+import com.cts.proj.service.AdminService;
 
+public class AdminValidator implements Validator {
+	
+    AdminService adminService;
 	@Override
 	public boolean supports(Class<?> clazz) {
 		// TODO Auto-generated method stub
-		return false;
+		
+		return Admin.class.equals(clazz);
 	}
 
 	@Override
 	public void validate(Object target, Errors errors) {
 		// TODO Auto-generated method stub
+		Admin admin = (Admin) target;
+		try {
+			if(!SecureWithSHA256.getSHA(admin.getTempPassword()).equals(adminService.getAdminPassword(admin.getAdminId())))
+				errors.rejectValue("tempPassword", "passwordError", "Enter the correct Password");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
