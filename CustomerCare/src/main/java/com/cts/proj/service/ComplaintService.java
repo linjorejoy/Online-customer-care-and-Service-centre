0ebde7041788fd597082fd115d76cf1af8dd0b01
AmazonianPanda <howyoudoing1618@gmail.com>
2020-12-05 14:58:45 +0530

    complaint service added

--- CustomerCare/src/main/java/com/cts/proj/service/ComplaintService.java
+++ CustomerCare/src/main/java/com/cts/proj/service/ComplaintService.java
@@ -1,5 +1,44 @@
 package com.cts.proj.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cts.proj.model.Complaint;
+import com.cts.proj.repository.ComplaintRepository;
+
+@Service
 public class ComplaintService {
 
+	@Autowired
+	ComplaintRepository complaintRepository;
+	
+	public boolean addComplaint(Complaint complaint) {
+		complaintRepository.save(complaint);
+		return true;
+	}
+	
+	public List<Complaint> getAllComplaint(){
+		return complaintRepository.findAll();
+	}
+	
+	public Complaint getComplaint(long complaintId) {
+		return complaintRepository.findById(complaintId).orElse(null);
+	}
+	
+	public boolean updateComplaint(Complaint complaint) {
+		complaintRepository.save(complaint);
+		return true;
+	}
+	
+	public boolean deleteComplaint(long complaintId) {
+		complaintRepository.deleteById(complaintId);;
+		return true;
+	}
+	
+	public boolean deleteComplaint(Complaint complaint) {
+		complaintRepository.delete(complaint);
+		return true;
+	}
 }
