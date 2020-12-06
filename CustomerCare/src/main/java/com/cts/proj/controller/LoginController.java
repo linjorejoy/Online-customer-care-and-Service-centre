7bb8453381b4acaf9c035950ecc1cf6429d8dbd7
AmazonianPanda <howyoudoing1618@gmail.com>
2020-12-06 10:54:44 +0530

    before changing model classes

--- CustomerCare/src/main/java/com/cts/proj/controller/LoginController.java
+++ CustomerCare/src/main/java/com/cts/proj/controller/LoginController.java
@@ -1,5 +1,5 @@
 package com.cts.proj.controller;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
