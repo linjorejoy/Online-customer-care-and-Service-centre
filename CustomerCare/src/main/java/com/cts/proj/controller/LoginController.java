ff9fc05e7450a9e6730c77fc1e542bdc4cdf1057
AmazonianPanda <howyoudoing1618@gmail.com>
2020-12-06 10:55:32 +0530

    login controller corrected

--- CustomerCare/src/main/java/com/cts/proj/controller/LoginController.java
+++ CustomerCare/src/main/java/com/cts/proj/controller/LoginController.java
@@ -1,5 +1,5 @@
 package com.cts.proj.controller;
- 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -11,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.cts.proj.model.Admin;
 import com.cts.proj.model.Analyst;
+import com.cts.proj.model.Complaint;
 import com.cts.proj.model.User;
 import com.cts.proj.service.ComplaintService;
+import com.cts.proj.validate.AdminPasswordValidator;
+import com.cts.proj.validate.AnalystPasswordValidator;
 import com.cts.proj.validate.UserPasswordValidator;
 
 @Controller
@@ -23,14 +26,22 @@ public class LoginController {
 	
 	@Autowired
 	UserPasswordValidator userPasswordValidator;
+	
+	@Autowired
+	AnalystPasswordValidator analystPasswordValidator;
+	
+	@Autowired
+	AdminPasswordValidator adminPasswordValidator;
 
 	@RequestMapping(value = "/user-login", method = RequestMethod.GET)
-	public String userLogin(@Validated @ModelAttribute("user") User user, BindingResult result) {
+	public String userLogin(@Validated @ModelAttribute("complaint") Complaint complaint, BindingResult result) {
 		return "user-login";
 	}
 
 	@RequestMapping(value = "/user-login", method = RequestMethod.POST)
-	public String userAfterLogin(@Validated @ModelAttribute("user") User user, BindingResult result) {
+	public String userAfterLogin(@Validated @ModelAttribute("complaint") Complaint complaint, BindingResult result) {
+		
+		userPasswordValidator.validate(complaint, result);
 		if (result.hasErrors()) {
 			return "user-login";
 		}
@@ -45,18 +56,26 @@ public class LoginController {
 	@RequestMapping(value = "/admin-login", method = RequestMethod.POST)
 	public String adminAfterLogin(@Validated @ModelAttribute("admin") Admin admin, BindingResult result,
 			ModelMap model) {
+		adminPasswordValidator.validate(admin, result);
+		if(result.hasErrors()) {
+			return "admin-login";
+		}
 		return "complaint-notification-admin";
 	}
 
 	@RequestMapping(value = "/analyst-login", method = RequestMethod.GET)
-	public String analystLogin(@Validated @ModelAttribute("analyst") Analyst analyst, BindingResult result) {
+	public String analystLogin(@Validated @ModelAttribute("complaint") Complaint complaint, BindingResult result) {
 		return "analyst-login";
 	}
 
 	@RequestMapping(value = "/analyst-login", method = RequestMethod.POST)
-	public String analystAfterLogin(@Validated @ModelAttribute("analyst") Analyst analyst, BindingResult result,
+	public String analystAfterLogin(@Validated @ModelAttribute("complaint") Complaint complaint, BindingResult result,
 			ModelMap model) {
-		return "complaint-notification-admin";
+		analystPasswordValidator.validate(complaint, result);
+		if(result.hasErrors()) {
+			return "analyst-login";
+		}
+		return "complaint-notification-analyst";
 	}
 
 }
