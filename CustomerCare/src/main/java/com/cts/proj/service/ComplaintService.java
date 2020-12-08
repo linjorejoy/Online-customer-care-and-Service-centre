45f61fcd2d597e9280b7e7baa67a59cfb96e06a2
AmazonianPanda <howyoudoing1618@gmail.com>
2020-12-08 11:27:18 +0530

    pagination of analyst page completed

--- CustomerCare/src/main/java/com/cts/proj/service/ComplaintService.java
+++ CustomerCare/src/main/java/com/cts/proj/service/ComplaintService.java
@@ -17,54 +17,65 @@ public class ComplaintService {
 
 	@Autowired
 	ComplaintRepository complaintRepository;
-	
+
 	public boolean addComplaint(Complaint complaint) {
 		complaintRepository.save(complaint);
 		return true;
 	}
-	
-	public Page<Complaint> getAllComplaint(int pageNumber, int count, String sortBy, String sortDir){
+
+	public Page<Complaint> getAllComplaint(int pageNumber, int count, String sortBy, String sortDir) {
 		Sort sort = null;
-		if(sortDir.equalsIgnoreCase("asc")) {
+		if (sortDir.equalsIgnoreCase("asc")) {
 			sort = Sort.by(sortBy).ascending();
-		}else if(sortDir.equalsIgnoreCase("desc")) {
+		} else if (sortDir.equalsIgnoreCase("desc")) {
 			sort = Sort.by(sortBy).descending();
-		}else {
-			sort = Sort.by(sortBy).ascending();			
+		} else {
+			sort = Sort.by(sortBy).ascending();
 		}
 		Pageable pageable = PageRequest.of(pageNumber, count, sort);
 		return complaintRepository.findAll(pageable);
 	}
-	
+
 	public Complaint getComplaint(long complaintId) {
 		return complaintRepository.findById(complaintId).orElse(null);
 	}
-	
+
 	public boolean updateComplaint(Complaint complaint) {
 		complaintRepository.save(complaint);
 		return true;
 	}
-	
+
 	public boolean deleteComplaint(long complaintId) {
-		complaintRepository.deleteById(complaintId);;
+		complaintRepository.deleteById(complaintId);
+		;
 		return true;
 	}
-	
+
 	public boolean deleteComplaint(Complaint complaint) {
 		complaintRepository.delete(complaint);
 		return true;
 	}
-	
-	public List<Complaint> getAllComplaintForAnalyst(long analystId){
-		List<Complaint> complaintListAnalyst = complaintRepository.findComplaintsByAnalyst(analystId);
-		
-		return complaintListAnalyst;
-		
+
+	public Page<Complaint> getAllComplaintForAnalyst(long analystId, int pageNumber, int count, String sortBy,
+			String sortDir) {
+		System.out.println("Analyst Id Service clsas : " + analystId);
+		Sort sort = null;
+		if (sortDir.equalsIgnoreCase("asc")) {
+			sort = Sort.by(sortBy).ascending();
+		} else if (sortDir.equalsIgnoreCase("desc")) {
+			sort = Sort.by(sortBy).descending();
+		} else {
+			sort = Sort.by(sortBy).ascending();
+		}
+		Pageable pageable = PageRequest.of(pageNumber, count, sort);
+		System.out.println("In service class : " + complaintRepository.findComplaintsByAnalyst(pageable, analystId).getContent());
+		return complaintRepository.findComplaintsByAnalyst(pageable, analystId);
+
 	}
 
 	public List<Complaint> getAllComplaintOfUser(long userId) {
 		// TODO Auto-generated method stub
-		
+
 		return complaintRepository.findComplaintForUser(userId);
 	}
 }
