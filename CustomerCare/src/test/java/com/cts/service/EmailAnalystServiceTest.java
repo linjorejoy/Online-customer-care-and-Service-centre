6d38a65e3ae441d49e78e5f4b75dee99acd40faf
AmazonianPanda <howyoudoing1618@gmail.com>
2020-12-10 09:22:36 +0530

    junit ver 4 removed

---
+++ CustomerCare/src/test/java/com/cts/service/EmailAnalystServiceTest.java
@@ -0,0 +1,14 @@
+package com.cts.service;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class EmailAnalystServiceTest {
+	
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
